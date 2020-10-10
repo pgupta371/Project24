1e3160f27954d26277250a9647d8dd95620aea12
pgupta371 <71049997+pgupta371@users.noreply.github.com>
2020-10-09 22:03:07 -0500

    Add files via upload

---
+++ PAPER.js
@@ -0,0 +1,29 @@
+class paper {
+    constructor(x, y, r) {
+      var options = {
+          //'isStatic':false,
+		    'restitution':0.5,
+          'friction':1,
+          'density':1.5
+      }
+    this.x=x;
+		this.y=y;
+		this.r=r
+		this.body=Bodies.circle(x,y,r/2, options)
+		World.add(world, this.body);
+
+      
+    }
+    display(){
+      var pos =this.body.position;
+      push();
+      translate(pos.x, pos.y);
+      ellipseMode(CENTER);
+      fill("white");
+      ellipse(0, 0, this.r, this.r);
+      pop();
+    }
+  }
+
+     
+   
---
+++ TRASH.js
@@ -0,0 +1,17 @@
+class trash {
+  constructor(x,y,width,height) {
+      var opt = {isStatic: true};
+      this.body = Bodies.rectangle(x,y,width,height, opt);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+  }
+  display() {
+      var pos = this.body.position;
+
+      rectMode(CENTER);
+      fill("gray");
+      rect(pos.x, pos.y, this.width, this.height);
+
+  }
+}
